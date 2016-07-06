c384c49ace34bfd7ac6304ad9cad9baa53ee2ecc
chucklapress <charleslapress@gmail.com>
2016-07-06 19:57:15 -0400

    changed class attr to souper, results would not print right with main.py in django

--- guitar_scraper/app/views.py
+++ guitar_scraper/app/views.py
@@ -6,15 +6,16 @@ from bs4 import BeautifulSoup
 # Create your views here.
 def guitar_scraping_view(request):
     song = request.GET.get('song') or "NIB"
-    artists = request.GET.get("song")
     url = "http://www.guitartabs.cc/search.php?tabtype=any&band=&song={}".format(song)
     content = requests.get(url).text
     souper = BeautifulSoup(content, "html.parser")
-    songs = souper.find_all(class_="ryzh22")
-
-    link = [link["href"] for link in souper.find_all(class_="ryzh22")]
+    songs = str(souper.find(class_="tabslist"))
 
 
 
 
-    return render(request,"index.html",{"link":link})
+
+
+
+
+    return render(request,"index.html",{"songs":songs})
