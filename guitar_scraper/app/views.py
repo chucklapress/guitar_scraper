783709cc87de40f47b8fe76c7b0e18f3348337b8
chucklapress <charleslapress@gmail.com>
2016-07-06 18:49:17 -0400

    changed url to search by song modified main.py then put into django

--- guitar_scraper/app/views.py
+++ guitar_scraper/app/views.py
@@ -5,10 +5,16 @@ from bs4 import BeautifulSoup
 
 # Create your views here.
 def guitar_scraping_view(request):
-    artists = request.GET.get("artists")
-    url = "http://www.guitartabs.cc/search.php?tabtype=any&band={}".format(artists)
+    song = request.GET.get('song') or "NIB"
+    artists = request.GET.get("song")
+    url = "http://www.guitartabs.cc/search.php?tabtype=any&band=&song={}".format(song)
     content = requests.get(url).text
     souper = BeautifulSoup(content, "html.parser")
-    
+    songs = souper.find_all(class_="ryzh22")
 
-    return render(request,"index.html",{"artists":artists,"bands":bands})
+    link = [link["href"] for link in souper.find_all(class_="ryzh22")]
+
+
+
+
+    return render(request,"index.html",{"link":link})
--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from bs4 import BeautifulSoup
 
 
 def guitar_scraping_view():
-    
-    url = "http://www.guitartabs.cc/tabs/b/black_sabbath/"
+
+    url = "http://www.guitartabs.cc/search.php?tabtype=any&band=&song=NIB"
     content = requests.get(url).text
     souper = BeautifulSoup(content, "html.parser")
-    songs = souper.find_all(class_="ryzh2")
+    songs = souper.find_all(class_="ryzh22")
     for song in songs:
         print(song.attrs["href"])
 
