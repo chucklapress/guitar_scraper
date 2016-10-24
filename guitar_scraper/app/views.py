09283f8c7be7eb8d61308abcf019542f8621d610
chucklapress <charleslapress@gmail.com>
2016-10-24 17:00:54 -0400

    added things to make it look better in base and index html

--- guitar_scraper/app/views.py
+++ guitar_scraper/app/views.py
@@ -9,7 +9,7 @@ from bs4 import BeautifulSoup
 
 
 def guitar_scraping_view(request):
-    song = request.GET.get('song') or "yesterday once more"
+    song = request.GET.get('song') or "Applause"
     url = "http://www.guitartabs.cc/search.php?tabtype=any&band=&song={}".format(song)
     content = requests.get(url).text
     souper = BeautifulSoup(content, "html.parser")
